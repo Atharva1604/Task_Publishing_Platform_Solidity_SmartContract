601de68a9c25bcca37e49202d4ab1dcf3f0bd199
Atharva Patil <67190272+Atharva1604@users.noreply.github.com>
2021-12-07 13:38:57 -0500

    Add files via upload

--- SmartContract.sol
+++ SmartContract.sol
@@ -2,13 +2,12 @@
 // OpenZeppelin Contracts v4.3.2 (token/ERC20/ERC20.sol)
 
 pragma solidity ^0.8.2;
-// import "TaskToken.sol";
 
-import "./TaskToken.sol";
+import "TaskToken.sol";
 
-contract TASK is ERC20{
+contract TASK is TaskToken
+{
     address _owner;
-    //IERC20 public token;
     modifier onlyOwner {
         require( msg.sender == _owner);
         _;
@@ -26,15 +25,12 @@ contract TASK is ERC20{
     }
    
     mapping(address => uint) A_token_balance;
-    mapping(address => Assignee) Assignee_details;
-    
+    mapping(address => Assignee) Assignee_details;    
     
     constructor()  {
         _owner = msg.sender;
-        // token = new ERC20();
     }
    
-
     struct jobmapping{
         string name;
         address Assignor;
@@ -45,20 +41,18 @@ contract TASK is ERC20{
     }
    
     mapping(uint => jobmapping) Jobs;
+    uint public job_counter = 0;
    
     function job_completed(uint _job_id, uint rating) public onlyAssignor(Jobs[_job_id].Assignor){
-        //payable(Jobs[_job_id].Assignee).transfer(Jobs[_job_id].cost);
-       transfer(address(this), Jobs[_job_id].Assignee, Jobs[_job_id].cost);
+       transferFrom(address(this), Jobs[_job_id].Assignee, Jobs[_job_id].cost);
        
         Assignee_details[Jobs[_job_id].Assignee].avg_rating =  ((Assignee_details[Jobs[_job_id].Assignee].avg_rating*Assignee_details[Jobs[_job_id].Assignee].no_of_jobs_completed) + rating)/(Assignee_details[Jobs[_job_id].Assignee].no_of_jobs_completed + 1);
         Assignee_details[Jobs[_job_id].Assignee].no_of_jobs_completed =  Assignee_details[Jobs[_job_id].Assignee].no_of_jobs_completed + 1;
-        //B_details[Jobs[_job_id].B].token_balance =  B_details[Jobs[_job_id].B].token_balance - Jobs[_job_id].cost;
        
     }
     function job_not_completed(uint _job_id) public onlyAssignor(Jobs[_job_id].Assignor){
        
-        // payable(Jobs[_job_id].Assignor).transfer(Jobs[_job_id].cost);
-        transferFrom(address(this), Jobs[_job_id].Assignor, totalSupply());
+        transferFrom(address(this), Jobs[_job_id].Assignor,Jobs[_job_id].cost);
         Jobs[_job_id].cost = 0;
        
     }
@@ -67,8 +61,9 @@ contract TASK is ERC20{
         return (Jobs[_job_id].name, Jobs[_job_id].cost,Jobs[_job_id].rating,Jobs[_job_id].ipfslink);
     }    
    
-    function add_job_details(uint _job_id, string memory QM_ipfslink, string memory name, uint value) public {
-        jobmapping storage job_det = Jobs[_job_id];
+    function add_job_details(string memory QM_ipfslink, string memory name, uint value) public {
+        job_counter = job_counter + 1;
+        jobmapping storage job_det = Jobs[job_counter];
         transfer(address(this), value);
         job_det.Assignor = msg.sender;
         approve(address(this),msg.sender,_totalSupply);
@@ -76,20 +71,21 @@ contract TASK is ERC20{
         job_det.name = name;
         job_det.ipfslink = QM_ipfslink;
         job_det.cost = value;
+        
        
+    }
+
+    function return_job_counter() public view returns (uint) {
+        return job_counter;
     }
    
     function add_assignee_details(uint _job_id, address assignee_address) public{
         jobmapping storage job_det = Jobs[_job_id];
         job_det.Assignee = assignee_address;
-        approve(address(this),  msg.-,_totalSupply);
+        approve(address(this),  msg.sender,_totalSupply);
         approve(msg.sender, address(this),_totalSupply);
     }
-   
-    //  function get_rating(uint _job_id) public view returns(uint){
-    //     return (Assignee_details[Jobs[_job_id].Assignee].avg_rating);
-    // }  
-   
+     
     function get_rating_address(address assignee_address) public view returns(uint){
         return (Assignee_details[assignee_address].avg_rating);
     }  
